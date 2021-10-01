fa282c7282f174586aa06477573dc31f384e8198
E15dev <85256998+E15dev@users.noreply.github.com>
2021-10-01 23:37:02 +0200

    pm but plus pickle module
    sorry but idk why tihs not work xd.
    same at 'pm', but i add pickle module for better security.

---
+++ code/pmp.py
@@ -0,0 +1,48 @@
+import os,pickle
+import _sha256 as sha
+
+
+def _clearbuffer():
+    bf = open('./buffer.txt', 'w')
+    bf.write('')
+    bf.close()
+
+
+def encode(username, password):
+    try:
+        _clearbuffer()
+        file = open('../pm/' + str(username) + '.txt', 'wb')
+        d = []
+        p = str(password).encode()
+        p = sha.sha256(p).digest()
+        d.append(str(p))
+        file.write(pickle.dump(d, file)) # TODO : FIX THIS
+        file.close()
+        return True
+    except:
+        return False
+
+
+def compare(username, password):
+    _clearbuffer()
+    bf = open('./buffer.txt', 'wb')
+    file = open('../pm/' + str(username) + '.txt', 'rb')
+    p = str(password).encode()
+    p = sha.sha256(p).digest()
+    d = []
+    d.append(str(p))
+    # tp = str(file.read())
+    bf.write(pickle.dump(d, bf)) # TODO : FIX THIS TOO
+    bf.close()
+    bf = open('./buffer.txt', 'rb')
+    if  bf.read() == file.read():
+        return True
+    else:
+        return False
+    return False
+
+
+if __name__  == '__main__':
+    encode('XerwinXpl', 'mypassowrd')
+    print(compare('XerwinXpl', 'mypassword'))
+    print(compare('XerwinXpl', 'anotherpassword'))
